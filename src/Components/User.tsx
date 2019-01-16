7e072480d2112d50fbfab58ef7c54084ec3173aa
Usama Aslam <usamaslam123@gmail.com>
2019-01-16 17:24:26 +0500

    Commited

---
+++ src/Components/User.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class User extends Component {
+  render() {
+    return (
+      <div className="User">
+        <h1>Hello From User</h1>
+      </div>
+    );
+  }
+}
+
+export default User;
